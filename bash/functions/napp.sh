d41456dde742cb2d1f4b2207a8bf85895c710eed
Renato Mascarenhas <renato.mascosta@gmail.com>
2012-06-04 17:07:22 -0300

    Add napp function
    napp function creates a new Rails app. Uses most recent rails version,
    and install gems at vendor/bundle.

---
+++ bash/functions/napp.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# napp creates a new Rails app for you
+# Assumptions:
+#   * Using bundler (d'oh!)
+#   * Use latest rails available
+
+function napp() {
+    if [[ "$#" == 0 ]]; then
+        echo "Gimme an app name as a parameter!"
+        return 1
+    fi
+
+    local appname=$1
+
+    mkdir "$appname"
+    cd "$appname"
+
+    # Write a simple Gemfile
+    echo "source :rubygems" > Gemfile
+    echo "gem 'rails'" >> Gemfile
+
+    # Install the rails!
+    bundle install --path vendor/bundle
+
+    # Create the app, overwriting Gemfile
+    yes n | bundle exec rails new .
+
+    # Have fun!
+}
